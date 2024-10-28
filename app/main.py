d1f7cb1c84a8c1b8012696134fd77798b4222556
Margaux Reynolds <tsh3ut@virginia.edu>
2024-10-27 22:38:55 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,10 +8,18 @@ import os
 
 app = FastAPI()
 
-@app.get("/")  # zone apex
+@app.get("/")  # zone apex (default url)
 def zone_apex():
     return {"Hello": "Hello API"}
 
 @app.get("/add/{a}/{b}")
 def add(a: int, b: int):
     return {"sum": a + b}
+
+@app.get("/multiply/{c}/{d}") #use different variables, dont use a and b again from previous one
+def multiply(c: int, d: int):
+    return {"product": c*d}
+
+@app.get("/square/{a}") # new endpoint added to square a number
+def square(a: int):
+    return {"result": a ** 2}
