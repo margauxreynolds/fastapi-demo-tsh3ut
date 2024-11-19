c825c96a7801704838f541309925ea74575b4f1d
Margaux Reynolds <tsh3ut@virginia.edu>
2024-11-18 22:49:33 -0500

    Modified main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,66 @@
 #!/usr/bin/env python3
-
+import mysql.connector
+from mysql.connector import Error
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
 import json
 import os
 
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "ds2022"
+DBPASS = os.getenv('DBPASS')
+DB = "tsh3ut"
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+cur=db.cursor()
+
+
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
+    
+@app.get('/songs')
+def get_songs():
+    query = """
+    SELECT 
+        songs.title, 
+        songs.album, 
+        songs.artist, 
+        songs.year, 
+        songs.file AS mp3_file,
+        genres.genre AS genre
+    FROM songs
+    JOIN genres ON songs.genre = genres.genreid;
+    """
+    try:
+        cur.execute(query) 
+        headers = [x[0] for x in cur.description] 
+        results = cur.fetchall()  
+        json_data = []
+        for result in results:
+            json_data.append(dict(zip(headers, result)))  
+        return json_data
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
 
 @app.get("/")  # zone apex (default url)
 def zone_apex():
