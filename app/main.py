20c24e9ec6675ccf52e1d24fa67e220554cc8e99
Margaux Reynolds <tsh3ut@virginia.edu>
2024-11-27 15:45:20 -0500

    update fastapi-demo-tsh3ut

--- app/main.py
+++ app/main.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
+
+from fastapi import FastAPI
+# from typing import Optional
+# from pydantic import BaseModel
 import mysql.connector
 from mysql.connector import Error
-from fastapi import FastAPI
-from typing import Optional
-from pydantic import BaseModel
 import json
 import os
 
 DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
-DBUSER = "ds2022"
+DBUSER = "admin"
 DBPASS = os.getenv('DBPASS')
 DB = "tsh3ut"
-db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
-cur=db.cursor()
 
+# db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+# cur=db.cursor()
 
 app = FastAPI()
 from fastapi.middleware.cors import CORSMiddleware
@@ -24,8 +25,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/")  # zone apex
+def zone_apex():
+    return {"Good Day": "Sunshine!"}
+
 @app.get('/genres')
-def get_genres():
+async def get_genres():
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB, ssl_disabled=True)
+    cur = db.cursor()
     query = "SELECT * FROM genres ORDER BY genreid;"
     try:    
         cur.execute(query)
@@ -34,50 +41,53 @@ def get_genres():
         json_data=[]
         for result in results:
             json_data.append(dict(zip(headers,result)))
+        cur.close()
+        db.close()
         return(json_data)
     except Error as e:
+        print("MySQL Error: ", str(e))
+        cur.close()
+        db.close()
         return {"Error": "MySQL Error: " + str(e)}
+
     
 @app.get('/songs')
-def get_songs():
-    query = """
-    SELECT 
-        songs.title, 
-        songs.album, 
-        songs.artist, 
-        songs.year, 
-        songs.file AS mp3_file,
-        genres.genre AS genre
-    FROM songs
-    JOIN genres ON songs.genre = genres.genreid;
-    """
+async def get_genres():
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB, ssl_disabled=True)
+    cur = db.cursor()
+    query = "SELECT songs.title, songs.album, songs.artist, songs.year, songs.file, songs.image, genres.genre FROM songs JOIN genres WHERE songs.genre = genres.genreid;"
     try:
-        cur.execute(query) 
-        headers = [x[0] for x in cur.description] 
-        results = cur.fetchall()  
-        json_data = []
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
         for result in results:
-            json_data.append(dict(zip(headers, result)))  
-        return json_data
+            json_data.append(dict(zip(headers,result)))
+        cur.close()
+        db.close()
+        return(json_data)
     except Error as e:
-        return {"Error": "MySQL Error: " + str(e)}
+        print("MySQL Error: ", str(e))
+        cur.close()
+        db.close()
+        return None
 
-@app.get("/")  # zone apex (default url)
-def zone_apex():
-    return {"Hello": "Hello API"}
+# @app.get("/")  # zone apex (default url)
+# def zone_apex():
+#     return {"Hello": "Hello API"}
 
-@app.get("/add/{a}/{b}")
-def add(a: int, b: int):
-    return {"sum": a + b}
+# @app.get("/add/{a}/{b}")
+# def add(a: int, b: int):
+#     return {"sum": a + b}
 
-@app.get("/multiply/{c}/{d}") #use different variables, dont use a and b again from previous one
-def multiply(c: int, d: int):
-    return {"product": c*d}
+# @app.get("/multiply/{c}/{d}") #use different variables, dont use a and b again from previous one
+# def multiply(c: int, d: int):
+#     return {"product": c*d}
 
-@app.get("/square/{a}") # new endpoint added to square a number
-def square(a: int):
-    return {"result": a ** 2}
+# @app.get("/square/{a}") # new endpoint added to square a number
+# def square(a: int):
+#     return {"result": a ** 2}
 
-@app.get("/cube/{a}") # another new endpoint added to cube number
-def cube(a: int):
-    return {"result": a ** 3}
+# @app.get("/cube/{a}") # another new endpoint added to cube number
+# def cube(a: int):
+#     return {"result": a ** 3}
